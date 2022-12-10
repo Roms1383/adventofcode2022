2d8d4a0d9e821ed7b17cb73c509dd61c070ad8c4
Roms1383 <romain.kelifa@gmail.com>
2022-12-10 22:10:30 +0700

    :recycle: simplify day 8 part 1

--- src/day_8.rs
+++ src/day_8.rs
@@ -1,33 +1,41 @@
+#![allow(dead_code)]
+
+use std::{collections::HashMap, rc::Rc};
+
+#[derive(Debug, Eq, Hash, PartialEq, Clone)]
+pub struct Position(usize, usize);
+
 #[derive(Debug, Clone)]
 pub struct Tree {
-    x: usize,
-    y: usize,
     size: usize,
+    surroundings: Rc<HashMap<Position, Tree>>,
+}
+
+impl Tree {
+    fn size(&self) -> usize {
+        self.size
+    }
 }
 
 #[derive(Debug)]
-pub struct Forest(Vec<Vec<Tree>>);
+pub struct Forest(HashMap<Position, Tree>);
 
 impl From<&str> for Forest {
     fn from(v: &str) -> Self {
-        let h = v.lines().clone().count();
-        let w = v.lines().clone().next().unwrap().len();
-        let mut row: Vec<Tree> = Vec::with_capacity(w);
-        let mut grid = Vec::with_capacity(h);
-        let mut y = 0;
-        for line in v.lines() {
+        let mut inner: HashMap<Position, Tree> = HashMap::new();
+        let reference = Rc::new(inner.clone());
+        for (y, line) in v.lines().enumerate() {
             for (x, c) in line.chars().enumerate() {
-                row.push(Tree {
-                    x,
-                    y,
-                    size: c.to_digit(10).expect("should be a digit") as usize,
-                })
+                inner.insert(
+                    Position(x, y),
+                    Tree {
+                        size: c.to_digit(10).expect("should be a digit") as usize,
+                        surroundings: Rc::clone(&reference),
+                    },
+                );
             }
-            grid.push(row.clone());
-            row.clear();
-            y += 1;
         }
-        Forest(grid)
+        Self(inner)
     }
 }
 
@@ -44,7 +52,7 @@ mod tests {
     #[test]
     fn parse() {
         let forest = Forest::from(INPUT);
-        assert_eq!(forest.0.get(0).unwrap().len(), 5);
-        assert_eq!(forest.0.len(), 5);
+        println!("{forest:#?}");
+        assert!(false);
     }
 }
