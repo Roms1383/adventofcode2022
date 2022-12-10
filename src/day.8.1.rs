7b3d4873ccd94b828de2e31f75a56b6e31d2b77c
Roms1383 <romain.kelifa@gmail.com>
2022-12-11 01:39:57 +0700

    :sparkles: day 8 part 1

--- src/day.8.1.rs
+++ src/day.8.1.rs
@@ -1,7 +1,17 @@
 use std::fs::read_to_string;
 
+use crate::day_8::Forest;
+
 mod day_8;
 
 fn main() {
     let puzzle = read_to_string("./day.8.txt").expect("cannot read puzzle.txt");
+    let forest = Forest::from(puzzle.as_str());
+    let mut visible_ones = 0;
+    for key in forest.positions() {
+        if forest.visible_from_any_direction(*key) {
+            visible_ones += 1;
+        }
+    }
+    println!("{visible_ones}");
 }
--- src/day_8.rs
+++ src/day_8.rs
@@ -1,6 +1,6 @@
 #![allow(dead_code)]
 
-use std::collections::HashMap;
+use std::collections::{hash_map::Keys, HashMap};
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
 pub struct Position {
@@ -21,12 +21,15 @@ impl From<(usize, usize)> for Position {
 }
 
 impl Position {
+    fn any_edge(&self, bounds: Bounds) -> bool {
+        self.x == 0 || self.x == bounds.width - 1 || self.y == 0 || self.y == bounds.height - 1
+    }
     fn edge(&self, toward: Direction, bounds: Bounds) -> bool {
         match toward {
             Direction::Left => self.x == 0,
             Direction::Right => self.x == bounds.width - 1,
-            Direction::Up => self.y == 0,
-            Direction::Down => self.y == bounds.height - 1,
+            Direction::Top => self.y == 0,
+            Direction::Bottom => self.y == bounds.height - 1,
         }
     }
     fn next(&self, toward: Direction) -> Position {
@@ -39,11 +42,11 @@ impl Position {
                 x: self.x + 1,
                 y: self.y,
             },
-            Direction::Up => Position {
+            Direction::Top => Position {
                 x: self.x,
                 y: self.y - 1,
             },
-            Direction::Down => Position {
+            Direction::Bottom => Position {
                 x: self.x,
                 y: self.y + 1,
             },
@@ -51,15 +54,15 @@ impl Position {
     }
 }
 
-#[derive(Debug, Clone, Copy)]
+#[derive(Debug, Clone, Copy, PartialEq, PartialOrd)]
 pub struct Size(usize);
 
 #[derive(Debug, Clone, Copy)]
 pub enum Direction {
     Left,
     Right,
-    Up,
-    Down,
+    Top,
+    Bottom,
 }
 
 #[derive(Debug)]
@@ -69,6 +72,9 @@ pub struct Forest {
 }
 
 impl Forest {
+    pub fn positions(&self) -> Keys<Position, Size> {
+        self.grid.keys()
+    }
     fn edge(&self, at: Position, toward: Direction) -> bool {
         at.edge(toward, self.bounds)
     }
@@ -78,11 +84,31 @@ impl Forest {
         }
         self.grid.get(&at.next(toward))
     }
+    fn size(&self, at: Position) -> &Size {
+        self.grid.get(&at).as_ref().expect("no tree at position")
+    }
     fn visible(&self, at: Position, toward: Direction) -> bool {
-        if self.edge(at, toward) {
+        if at.any_edge(self.bounds) {
             return true;
         }
-        todo!()
+        let neighbors = self.neighbors(at, toward);
+        let size = self.size(at);
+        neighbors.iter().all(|x| *x < size)
+    }
+    pub fn visible_from_any_direction(&self, at: Position) -> bool {
+        if self.visible(at, Direction::Top) {
+            return true;
+        }
+        if self.visible(at, Direction::Bottom) {
+            return true;
+        }
+        if self.visible(at, Direction::Left) {
+            return true;
+        }
+        if self.visible(at, Direction::Right) {
+            return true;
+        }
+        false
     }
     fn neighbors(&self, at: Position, toward: Direction) -> Vec<&Size> {
         if self.edge(at, toward) {
@@ -152,10 +178,101 @@ mod tests {
         let neighbors = forest.neighbors(at.into(), Direction::Right);
         assert_eq!(neighbors.len(), 3);
         let at = (0, 3);
-        let neighbors = forest.neighbors(at.into(), Direction::Up);
+        let neighbors = forest.neighbors(at.into(), Direction::Top);
         assert_eq!(neighbors.len(), 3);
         let at = (0, 3);
-        let neighbors = forest.neighbors(at.into(), Direction::Down);
+        let neighbors = forest.neighbors(at.into(), Direction::Bottom);
         assert_eq!(neighbors.len(), 1);
+    }
+
+    #[test]
+    fn visibility() {
+        let forest = Forest::from(INPUT);
+        let at = (0, 0);
+        assert!(forest.visible(at.into(), Direction::Left));
+        assert!(forest.visible(at.into(), Direction::Right));
+        assert!(forest.visible(at.into(), Direction::Top));
+        assert!(forest.visible(at.into(), Direction::Bottom));
+
+        let at = (0, 4);
+        assert!(forest.visible(at.into(), Direction::Left));
+        assert!(forest.visible(at.into(), Direction::Right));
+        assert!(forest.visible(at.into(), Direction::Top));
+        assert!(forest.visible(at.into(), Direction::Bottom));
+
+        // top-left 5
+        let at = (1, 1);
+        assert!(forest.visible(at.into(), Direction::Left));
+        assert!(forest.visible(at.into(), Direction::Top));
+        assert!(!forest.visible(at.into(), Direction::Right));
+        assert!(!forest.visible(at.into(), Direction::Bottom));
+
+        // top-middle 5
+        let at = (2, 1);
+        assert!(forest.visible(at.into(), Direction::Top));
+        assert!(forest.visible(at.into(), Direction::Right));
+        assert!(!forest.visible(at.into(), Direction::Bottom));
+        assert!(!forest.visible(at.into(), Direction::Left));
+
+        // top-right 1
+        let at = (3, 1);
+        assert!(!forest.visible(at.into(), Direction::Top));
+        assert!(!forest.visible(at.into(), Direction::Right));
+        assert!(!forest.visible(at.into(), Direction::Bottom));
+        assert!(!forest.visible(at.into(), Direction::Left));
+
+        // left-middle 5
+        let at = (1, 2);
+        assert!(forest.visible(at.into(), Direction::Right));
+        assert!(!forest.visible(at.into(), Direction::Top));
+        assert!(!forest.visible(at.into(), Direction::Bottom));
+        assert!(!forest.visible(at.into(), Direction::Left));
+
+        // center 3
+        let at = (2, 2);
+        assert!(!forest.visible(at.into(), Direction::Top));
+        assert!(!forest.visible(at.into(), Direction::Right));
+        assert!(!forest.visible(at.into(), Direction::Bottom));
+        assert!(!forest.visible(at.into(), Direction::Left));
+
+        // right-middle 3
+        let at = (3, 2);
+        assert!(forest.visible(at.into(), Direction::Right));
+        assert!(!forest.visible(at.into(), Direction::Top));
+        assert!(!forest.visible(at.into(), Direction::Bottom));
+        assert!(!forest.visible(at.into(), Direction::Left));
+
+        // bottom-middle 5
+        let at = (2, 3);
+        assert!(!forest.visible(at.into(), Direction::Top));
+        assert!(!forest.visible(at.into(), Direction::Right));
+        assert!(forest.visible(at.into(), Direction::Bottom));
+        assert!(forest.visible(at.into(), Direction::Left));
+
+        // bottom-left 3
+        let at = (1, 3);
+        assert!(!forest.visible(at.into(), Direction::Top));
+        assert!(!forest.visible(at.into(), Direction::Right));
+        assert!(!forest.visible(at.into(), Direction::Bottom));
+        assert!(!forest.visible(at.into(), Direction::Left));
+
+        // bottom-right 4
+        let at = (3, 3);
+        assert!(!forest.visible(at.into(), Direction::Top));
+        assert!(!forest.visible(at.into(), Direction::Right));
+        assert!(!forest.visible(at.into(), Direction::Bottom));
+        assert!(!forest.visible(at.into(), Direction::Left));
+    }
+
+    #[test]
+    fn visibility_from_any_direction() {
+        let forest = Forest::from(INPUT);
+        let mut visible_ones = 0;
+        for key in forest.grid.keys() {
+            if forest.visible_from_any_direction(*key) {
+                visible_ones += 1;
+            }
+        }
+        assert_eq!(visible_ones, 21);
     }
 }
