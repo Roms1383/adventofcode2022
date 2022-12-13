35f1d87f1e3906f811dcb38ec47d64f4927967f6
Roms1383 <romain.kelifa@gmail.com>
2022-12-13 21:05:10 +0700

    :construction: wip day 10 part 2

--- src/day_10.rs
+++ src/day_10.rs
@@ -1,6 +1,87 @@
 #![allow(dead_code)]
 
-use std::collections::VecDeque;
+use std::{collections::VecDeque, fmt::Display};
+
+pub trait Cadence: Sized {
+    fn start(&mut self);
+    fn during(&mut self);
+    fn after(&mut self);
+}
+
+pub trait Draw {
+    fn draw(&mut self, cpu: &CPU, cycle: &Cycle);
+}
+
+#[derive(Debug, Clone, Copy)]
+pub struct Pixel(u8);
+
+impl Pixel {
+    fn low() -> Self {
+        Self(0)
+    }
+    fn high() -> Self {
+        Self(1)
+    }
+}
+
+pub struct Sprite {
+    cycle: Cycle,
+    cpu: CPU,
+}
+
+impl From<(&Cycle, &CPU)> for Sprite {
+    fn from((cycle, cpu): (&Cycle, &CPU)) -> Self {
+        Self {
+            cycle: cycle.clone(),
+            cpu: cpu.clone(),
+        }
+    }
+}
+
+impl Sprite {
+    fn middle(&self) -> isize {
+        self.cpu.0
+    }
+    fn visible(&self) -> bool {
+        self.middle() == self.cycle.pixel_row_position() as isize
+            || (self.middle() - 1) == self.cycle.pixel_row_position() as isize
+            || (self.middle() + 1) == self.cycle.pixel_row_position() as isize
+    }
+    fn produce(&self) -> Pixel {
+        if self.visible() {
+            return Pixel::high();
+        }
+        Pixel::low()
+    }
+    fn column(&self) -> usize {
+        self.cycle.pixel_colum_position()
+    }
+}
+
+#[derive(Debug, Clone)]
+pub struct CRT([[Pixel; 40]; 6]);
+
+impl Default for CRT {
+    fn default() -> Self {
+        Self([[Pixel::low(); 40]; 6])
+    }
+}
+
+impl Draw for CRT {
+    fn draw(&mut self, cpu: &CPU, cycle: &Cycle) {
+        let sprite: Sprite = (cycle, cpu).into();
+        self.0[sprite.column()][cycle.pixel_row_position()] = sprite.produce();
+        println!(
+            "{} at cycle {:#?}",
+            self.0[sprite.column()]
+                .iter()
+                .map(|x| x.0.to_string())
+                .collect::<Vec<String>>()
+                .join(", "),
+            cycle
+        );
+    }
+}
 
 #[derive(Debug, Clone, Copy)]
 pub struct Cycle(usize);
@@ -9,12 +90,12 @@ impl Cycle {
     fn relevant(&self) -> bool {
         [20, 60, 100, 140, 180, 220].contains(&self.0)
     }
-}
-
-pub trait Cadence: Sized {
-    fn start(&mut self);
-    fn during(&mut self);
-    fn after(&mut self);
+    fn pixel_row_position(&self) -> usize {
+        (self.0 - 1) % 40
+    }
+    fn pixel_colum_position(&self) -> usize {
+        (self.0 - 1) / 40
+    }
 }
 
 impl Cadence for Clock {
@@ -26,6 +107,7 @@ impl Cadence for Clock {
     }
 
     fn during(&mut self) {
+        self.draw(&self.cpu.clone(), &self.cycle.clone());
         if self.cycle.relevant() {
             self.signals.push((self.cycle, self.cpu))
         }
@@ -46,9 +128,16 @@ impl Cadence for Clock {
     }
 }
 
+impl Draw for Clock {
+    fn draw(&mut self, cpu: &CPU, cycle: &Cycle) {
+        self.crt.draw(cpu, cycle);
+    }
+}
+
 pub struct Clock {
     cpu: CPU,
     cycle: Cycle,
+    crt: CRT,
     busy: usize,
     current: Option<Instruction>,
     instructions: Instructions,
@@ -60,6 +149,7 @@ impl Clock {
         Self {
             cpu: Default::default(),
             cycle: Default::default(),
+            crt: Default::default(),
             busy: 0,
             current: None,
             instructions,
@@ -80,6 +170,21 @@ impl Clock {
         self.signals
             .iter()
             .fold(0, |acc, (cycle, cpu)| acc + (cycle.0 as isize * cpu.0))
+    }
+
+    pub fn draw_picture(&self) -> String {
+        let mut buf = String::from("");
+        for line in self.crt.0.iter() {
+            for pixel in line.iter() {
+                match pixel {
+                    &Pixel(0) => buf.push_str("."),
+                    &Pixel(1) => buf.push_str("#"),
+                    _ => panic!("should not happen"),
+                };
+            }
+            buf.push_str("\n");
+        }
+        buf
     }
 }
 
